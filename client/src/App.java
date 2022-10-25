1e7e2bcd77f596a64fb40158be61609bc9d03307
hihihaha <hieutedam2k2@gmail.com>
2022-10-25 08:34:30 +0700

    connected

--- client/src/App.java
+++ client/src/App.java
@@ -1,9 +1,25 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class App {
     public static void main(String[] args) throws IOException {
         Socket s = new Socket("localhost", 4999);
         System.out.println("Hello, World!");
+
+        // send data to server
+        PrintWriter pr = new PrintWriter(s.getOutputStream());
+        pr.println("Hello server");
+        pr.flush();
+
+        // receive data from server
+        InputStreamReader in = new InputStreamReader(s.getInputStream());
+        BufferedReader bf = new BufferedReader(in);
+
+        String str = bf.readLine();
+        System.out.println("Server: " + str);
+        s.close();
     }
 }
--- server/src/App.java
+++ server/src/App.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -7,5 +10,19 @@ public class App {
         ServerSocket ss = new ServerSocket(4999);
         Socket s = ss.accept();
         System.out.println("Client connected");
+
+        // receive data from client
+        InputStreamReader in = new InputStreamReader(s.getInputStream());
+        BufferedReader bf = new BufferedReader(in);
+
+        String str = bf.readLine();
+        System.out.println("Client :" + str);
+
+        // send data to client
+        PrintWriter pr = new PrintWriter(s.getOutputStream());
+        pr.println("yes");
+        pr.flush();
+
+        ss.close();
     }
 }
