bfe97326b6ffdcecfccee94cd9657f77835ea568
hihihaha <hieutedam2k2@gmail.com>
2022-10-25 09:19:40 +0700

    server done

---
+++ server/src/UserThread.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class UserThread extends Thread {
+    private Socket socket;
+    private ChatServer server;
+    private PrintWriter writer;
+
+    public UserThread(Socket socket, ChatServer chatServer) {
+        this.socket = socket;
+        this.server = chatServer;
+    }
+
+    public void run() {
+        try {
+            InputStream in = socket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+            OutputStream out = socket.getOutputStream();
+            writer = new PrintWriter(out, true);
+
+            printUsers();
+
+            String userName = reader.readLine();
+            server.addUserName(userName);
+
+            String serverMessage = "New user connected: " + userName;
+            server.broadcast(serverMessage, this);
+
+            String clientMessage;
+            do {
+                clientMessage = reader.readLine();
+                serverMessage = "[" + userName + "]: " + clientMessage;
+                server.broadcast(serverMessage, this);
+
+            } while (!clientMessage.equals("bye"));
+            server.removeUser(userName, this);
+            socket.close();
+
+            serverMessage = userName + "has quitted";
+            server.broadcast(serverMessage, this);
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+    /**
+     * Sends a list of online users to the newly connected user.
+     */
+    void printUsers() {
+        if (server.hasUsers()) {
+            writer.println("Connected users: " + server.getUserNames());
+        } else {
+            writer.println("No other users connected");
+        }
+    }
+
+    /**
+     * Sends a message to the client.
+     */
+    void sendMessage(String message) {
+        writer.println(message);
+    }
+}
